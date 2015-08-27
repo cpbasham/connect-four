62c3f570ba3ee5083cc54c6223ddfe53d1a6fa51
Cameron Basham <cbasham@gm.slc.edu>
2015-08-27 15:10:40 -0700

    programatically generate board

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
+(function createBoard() {
+  var board = $("table#board");
+  for (var ri=0; ri<6; ri++) {
+    var tr = $("<tr>");
+    var rowCls = "row-" + ri;
+    for (var ci=0; ci<7; ci++) {
+      var cls = rowCls + " col-" + ci;
+      var td = $("<td class='" + cls + "'>");
+      tr.append(td);
+    }
+    board.append(tr);
+  }
+})();
+
+
 });
