45baf14f3cfa073eaae943dd4f4ecb7ab235c76d
Jason Xu &   "name": null <pair+1UnboundedSentience.benvogcodes@devbootcamp.com>
2015-08-28 10:08:29 -0700

    working hor, vert, top/down diag checker

--- public/js/application.js
+++ public/js/application.js
@@ -3,16 +3,23 @@ $(document).ready(function() {
 (function createBoard() {
   var board = $("table#board");
   for (var ri=0; ri<6; ri++) {
-    var tr = $("<tr>");
-    var rowCls = "row-" + ri;
+    var tr = $('<tr>');
+    var rowData = "data-row=" + ri;
     for (var ci=0; ci<7; ci++) {
-      var cls = rowCls + " col-" + ci;
+    var colData = "data-col=" + ci;
       var td = $("<td class='" + cls + "'>");
       tr.append(td);
     }
     board.append(tr);
   }
 })();
+
+
+$('table#board').on("click", "td", function(e) {
+  var target = $(e.target);
+});
+ //run winchecker after setting each piece
+
 
 
 });
